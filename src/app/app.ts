0d72ccc36f87f2d980ac948fbf7da48b37d9c4c5
NozimaOrinboyeva <orinboyevanozima275@gmail.com>
2025-07-17 18:57:22 +0500

    angularni boshlash

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { CarList } from './car-list/car-list';
+
+@Component({
+  selector: 'app-root',
+  template: ` <app-car-list></app-car-list> `,
+  imports: [CarList],
+})
+export class App {
+  protected title = ' angular-course';
+}
---
+++ src/app/car-list/car-list.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { Car } from '../models/car';
+import { Data } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-car-list',
+  imports: [ FormsModule],
+  templateUrl: './car-list.html',
+  styleUrl: './car-list.css'
+})
+export class CarList {
+  carName: string = '';
+  carYear: Date = new Date();
+
+  cars: Car[] = [];
+
+  
+  // car: Car = {
+  //   id: 1,
+  //   name: 'Jentra',
+  //   year: new Date('2025-06-07'),
+  // };
+
+  onSubmit(){
+
+    if(this.carName.length != 0 && this.carYear) {
+      const newCar: Car = {
+        id: this.cars.length + 1,
+        name: this.carName,
+        year: this.carYear,
+      };
+      this.cars.push(newCar);
+      this.carName = '';
+      this.carYear = new Date();
+      console.log(this.cars);
+    }
+    // console.log('Form submitted');
+    // console.log('Car Name:', this.carName);
+    // console.log('Car Year:', this.carYear);
+  }
+
+  onDelete(car: Car) {
+    this.cars = this.cars.filter((c) => c.id ! == car.id);
+  }
+
+}
